0f99e75d7b9fb5c0958c195cfd924691ae42877c
Nguyen Chinh <nguyenthanhchinh96@gmail.com>
2018-07-11 16:10:52 +0700

    init project

---
+++ app/src/main/java/com/example/nguyenchinh/androidcalculator/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.nguyenchinh.androidcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
